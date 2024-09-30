81b6b497b3e07eb2ed0072b69c5ce7e43c711a50
mariem <mariem.kanzari@esprit.tn>
2024-09-30 17:05:32 +0100

    Interface Bloc

---
+++ src/main/java/com/example/projetdevops/Services/Bloc/IBlocService.java
@@ -0,0 +1,23 @@
+package com.example.projetdevops.Services.Bloc;
+
+import com.example.projetdevops.DAO.Entities.Bloc;
+
+import java.util.List;
+
+public interface IBlocService {
+    Bloc addOrUpdate(Bloc b);
+    Bloc addOrUpdate2(Bloc b);
+
+    List<Bloc> findAll();
+
+    Bloc findById(long id);
+
+    void deleteById(long id);
+
+    void delete(Bloc b);
+
+    Bloc affecterChambresABloc(List<Long> numChambre, String nomBloc);
+    Bloc affecterBlocAFoyer( String nomBloc,  String nomFoyer) ;
+
+
+}
