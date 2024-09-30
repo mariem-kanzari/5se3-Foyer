d2e1309a4b654b3c2b0ed8b838bfe9bd83064ef9
ghofranetayari <ghofrane.tayari@esprit.tn>
2024-09-30 17:18:14 +0100

    Universite Service

---
+++ src/main/java/com/example/projetdevops/Services/Universite/UniversiteService.java
@@ -0,0 +1,40 @@
+package com.example.projetdevops.Services.Universite;
+
+import com.example.projetdevops.DAO.Entities.Universite;
+import com.example.projetdevops.DAO.Repositories.UniversiteRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class UniversiteService implements IUniversiteService {
+    UniversiteRepository repo;
+
+    @Override
+    public Universite addOrUpdate(Universite u) {
+        return repo.save(u);
+    }
+
+    @Override
+    public List<Universite> findAll() {
+        return repo.findAll();
+    }
+
+    @Override
+    public Universite findById(long id) {
+        return repo.findById(id).get();
+    }
+
+    @Override
+    public void deleteById(long id) {
+        repo.deleteById(id);
+    }
+
+    @Override
+    public void delete(Universite u) {
+        repo.delete(u);
+    }
+}
+
