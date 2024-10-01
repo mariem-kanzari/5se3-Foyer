f9416e9c8d100be872b078ff01f614e04dab6fbb
stastix <designab33@gmail.com>
2024-10-01 14:20:39 +0100

    correction

--- src/main/java/com/example/projetdevops/DAO/Entities/Reservation.java
+++ src/main/java/com/example/projetdevops/DAO/Entities/Reservation.java
@@ -1,58 +1,30 @@
-package tn.esprit.spring.RestControllers;
+package tn.esprit.spring.DAO.Entities;
 
-import lombok.AllArgsConstructor;
-import org.springframework.web.bind.annotation.*;
-import tn.esprit.spring.DAO.Entities.Reservation;
-import tn.esprit.spring.DAO.Entities.Universite;
-import tn.esprit.spring.Services.Reservation.IReservationService;
-import tn.esprit.spring.Services.Universite.IUniversiteService;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.FieldDefaults;
 
+import java.io.Serializable;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
-@RestController
-@RequestMapping("reservation")
+@Entity
+@Table(name = "T_RESERVATION")
+@Getter
+@Setter
+@NoArgsConstructor
 @AllArgsConstructor
-public class ReservationRestController {
-    IReservationService service;
+@Builder
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class Reservation implements Serializable {
+    @Id
+    String idReservation;
+    LocalDate anneeUniversitaire;
+    boolean estValide;
+    @ManyToMany
+    @JsonIgnore
+    List<Etudiant> etudiants = new ArrayList<>();
 
-    @PostMapping("addOrUpdate")
-    Reservation addOrUpdate(@RequestBody Reservation r) {
-        return service.addOrUpdate(r);
-    }
-
-    @GetMapping("findAll")
-    List<Reservation> findAll() {
-        return service.findAll();
-    }
-
-    @GetMapping("findById")
-    Reservation findById(@RequestParam String id) {
-        return service.findById(id);
-    }
-
-    @DeleteMapping("delete")
-    void delete(@RequestBody Reservation r) {
-        service.delete(r);
-    }
-
-    @DeleteMapping("deleteById")
-    void deleteById(@RequestParam String id) {
-        service.deleteById(id);
-    }
-
-    @PostMapping("ajouterReservationEtAssignerAChambreEtAEtudiant")
-    Reservation ajouterReservationEtAssignerAChambreEtAEtudiant(@RequestParam Long numChambre, @RequestParam long cin) {
-        return service.ajouterReservationEtAssignerAChambreEtAEtudiant(numChambre, cin);
-    }
-
-    @GetMapping("getReservationParAnneeUniversitaire")
-    long getReservationParAnneeUniversitaire(@RequestParam LocalDate debutAnnee, @RequestParam LocalDate finAnnee) {
-        return service.getReservationParAnneeUniversitaire(debutAnnee, finAnnee);
-    }
-
-    @DeleteMapping("annulerReservation")
-    String annulerReservation(@RequestParam long cinEtudiant) {
-        return service.annulerReservation(cinEtudiant);
-    }
 }
