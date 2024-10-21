6d057d74054187a966ea810e68521470ef5ce2a9
mariem <mariem.kanzari@esprit.tn>
2024-10-21 16:31:59 +0100

    correction pour sonar

--- src/main/java/com/example/projetdevops/DAO/Repositories/BlocRepository.java
+++ src/main/java/com/example/projetdevops/DAO/Repositories/BlocRepository.java
@@ -4,8 +4,9 @@ import com.example.projetdevops.DAO.Entities.TypeChambre;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param; 
+import org.springframework.data.repository.query.Param;
 import java.util.List;
+
 
 public interface BlocRepository extends JpaRepository<Bloc,Long> {
     Bloc findByNomBloc(String nom);
