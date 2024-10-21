3f84992e8e2e99bf440b0e87c44c3625d2131882
mariem <mariem.kanzari@esprit.tn>
2024-10-21 16:08:51 +0100

    correction pour sonar

--- src/main/java/com/example/projetdevops/RestControllers/BlocRestController.java
+++ src/main/java/com/example/projetdevops/RestControllers/BlocRestController.java
@@ -5,7 +5,6 @@ import com.example.projetdevops.Services.Bloc.IBlocService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
-
 import java.util.List;
 
 @RestController
@@ -15,37 +14,37 @@ public class BlocRestController {
     IBlocService service;
 
     @PostMapping("addOrUpdate")
-    Bloc addOrUpdate(@RequestBody Bloc b) {
+    public Bloc addOrUpdate(@RequestBody Bloc b) {
         return service.addOrUpdate(b);
     }
 
     @GetMapping("findAll")
-    List<Bloc> findAll() {
+    public List<Bloc> findAll() {
         return service.findAll();
     }
 
     @GetMapping("findById")
-    Bloc findById(@RequestParam long id) {
+    public Bloc findById(@RequestParam long id) {
         return service.findById(id);
     }
 
     @DeleteMapping("delete")
-    void delete(@RequestBody Bloc b) {
+    public void delete(@RequestBody Bloc b) {
         service.delete(b);
     }
 
     @DeleteMapping("deleteById")
-    void deleteById(@RequestParam long id) {
+    public void deleteById(@RequestParam long id) {
         service.deleteById(id);
     }
 
     @PutMapping("affecterChambresABloc")
-    Bloc affecterChambresABloc(@RequestBody List<Long> numChambre, @RequestParam String nomBloc) {
+    public Bloc affecterChambresABloc(@RequestBody List<Long> numChambre, @RequestParam String nomBloc) {
         return service.affecterChambresABloc(numChambre, nomBloc);
     }
 
     @PutMapping("affecterBlocAFoyer")
-    Bloc affecterBlocAFoyer(@RequestParam String nomBloc,@RequestParam String nomFoyer){
-        return service.affecterBlocAFoyer(nomBloc,nomFoyer);
+    public Bloc affecterBlocAFoyer(@RequestParam String nomBloc, @RequestParam String nomFoyer) {
+        return service.affecterBlocAFoyer(nomBloc, nomFoyer);
     }
 }
