97a33e684edf2999556a200cdb18d60d77922ee5
ghofranetayari <ghofrane.tayari@esprit.tn>
2024-11-09 11:50:37 +0100

    testsssss

--- src/test/java/com/example/projetdevops/BlocTests/BlocServiceTest.java
+++ src/test/java/com/example/projetdevops/BlocTests/BlocServiceTest.java
@@ -14,6 +14,7 @@ import jakarta.transaction.Transactional;
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -25,6 +26,8 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 
 @ExtendWith(SpringExtension.class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
--- src/test/java/com/example/projetdevops/EtudiantTests/EtdiantServiceTest.java
+++ src/test/java/com/example/projetdevops/EtudiantTests/EtdiantServiceTest.java
@@ -6,6 +6,7 @@ import com.example.projetdevops.Services.Etudiant.EtudiantService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import java.util.List;
@@ -13,6 +14,8 @@ import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+
 public class EtdiantServiceTest {
 
     @Autowired
