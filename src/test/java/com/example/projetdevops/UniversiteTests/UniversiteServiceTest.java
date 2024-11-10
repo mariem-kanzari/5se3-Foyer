1e4a18b2db36f523cdc1e88f572f58f0216e6e7e
ghofranetayari <115803997+ghofranetayari@users.noreply.github.com>
2024-11-10 21:12:46 +0100

    Update UniversiteServiceTest.java

--- src/test/java/com/example/projetdevops/UniversiteTests/UniversiteServiceTest.java
+++ src/test/java/com/example/projetdevops/UniversiteTests/UniversiteServiceTest.java
@@ -23,9 +23,8 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @ExtendWith(SpringExtension.class)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-@TestPropertySource(properties = "spring.test.context.timeout=10000") // 10 seconds timeout
-
-@SpringBootTest
+@TestPropertySource(properties = "spring.test.context.timeout=10000")  // 10 seconds timeout
+@SpringBootTest(properties = "spring.test.context.failure-threshold=2")
 @Transactional // This ensures tests are wrapped in a transaction
 public class UniversiteServiceTest {
 
