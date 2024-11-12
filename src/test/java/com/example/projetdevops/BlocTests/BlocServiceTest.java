d50ceb7384db05ce82754550396829cea2569055
mariem <mariem.kanzari@esprit.tn>
2024-11-12 09:14:01 +0100

    testing webhook

--- src/test/java/com/example/projetdevops/BlocTests/BlocServiceTest.java
+++ src/test/java/com/example/projetdevops/BlocTests/BlocServiceTest.java
@@ -12,11 +12,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
-
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 
